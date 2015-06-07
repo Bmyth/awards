dd92eb814ba14cb18e600aabc00b578f60033337
bmy <mingming.bai@yahoo.com>
2015-06-07 17:47:48 +0800

    bmm - award proto

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(function(){
+	$(".guest-list").owlCarousel({
+	  navigation : true,
+	  autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+	  items : 4,
+	  itemsDesktop : [1199,3],
+	  itemsDesktopSmall : [979,3]
+
+	});
+
+	$(".award-pick").click(function(){
+		var idx = $(this).attr('idx');
+		$(".award-list").each(function(){
+			if($(this).attr("idx") == idx){
+				$(this).show();
+			}else{
+				$(this).hide();
+			}
+		})
+	})
+})
